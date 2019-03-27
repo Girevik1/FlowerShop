f62cc107f4c39ea51b243f23f5553e0ff67c68b8
Arthur <46501109+Girevik1@users.noreply.github.com>
2019-03-27 13:49:54 +0300

    Add files via upload

---
+++ config/db_params.php
@@ -0,0 +1,9 @@
+<?php
+
+// Массив с параметрами подключения к базе данных
+return array(
+    'host' => 'localhost',
+    'dbname' => 'a0277211_shop',
+    'user' => 'a0277211_shop',
+    'password' => '78694636',
+);
